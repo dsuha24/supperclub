7c5e1da89dc987494e648065e6447779b2f27d41
Daryl Suharli <suharli@usc.edu>
2020-04-23 20:43:35 -0700

    Co-authored-by: Gabriel Ferraz <gabrielferraz27@gmail.com>

--- src/recipes/containers/Recipes.js
+++ src/recipes/containers/Recipes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import RecipeList from '../components/RecipeList';
-import RecipeModal from '../components/UIElements/RecipeModal';
+// import RecipeModal from '../components/UIElements/RecipeModal';
+import FilterBar from '../../layout/navigation/Filters/FilterBar';
 
 import "./Recipes.css";
 // import RecipeInstructions from './RecipeInstructions';
 
-const Recipes = () => {
+const Recipes = props => {
   const RECIPES = [
     {
       id: 'u1',
@@ -17,7 +18,67 @@ const Recipes = () => {
       time: 3,
       cost: 2,
       description: "This impressive french pastry is off the tits",
-      author: "Daryl Suharli"
+      author: "Daryl Suharli",
+      cuisine: "French",
+      ingredients: [
+        {
+            ingredient: 'Bread Flour',
+            amount: 3,
+            units: 'Cups',
+            subs: '3'
+        },
+        {
+            ingredient: 'Instant Yeast',
+            amount: '1/3',
+            units: 'Tsp',
+            subs: 2
+        },
+        {
+            ingredient: 'Euro Butter',
+            amount: 1,
+            units: 'Cups',
+            subs: 1
+        }
+      ],
+      steps: [
+        {
+            step: 1,
+            shortDescription: "Mix wet and dry ingredients",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+        {
+            step: 2,
+            shortDescription: "Mix wet and dry ingredients",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+      ],
+      equipmentTable: [
+        {
+            equipment: 'Stand Mixer',
+            subs: 2,
+            equipmentSteps: '1'
+        },
+        {
+            equipment: 'Rolling Pin',
+            subs: 2,
+            equipmentSteps: '2, 3, 4'
+        },
+        {
+            equipment: 'Plastic Wrap',
+            subs: 1,
+            equipmentSteps: '2, 4, 5'
+        },
+      ]
     },
     {
       id: 'u2',
@@ -28,7 +89,67 @@ const Recipes = () => {
       time: 3,
       cost: 2,
       description: "Better than the cronut imho",
-      author: "Dominique Ansel"
+      author: "Dominique Ansel",
+      cuisine: "French",
+      ingredients: [
+        {
+            ingredient: 'TEST DKA',
+            amount: 3,
+            units: 'Cups',
+            subs: '3'
+        },
+        {
+            ingredient: 'Instant Yeast',
+            amount: '1/3',
+            units: 'Tsp',
+            subs: 2
+        },
+        {
+            ingredient: 'Euro Butter',
+            amount: 1,
+            units: 'Cups',
+            subs: 1
+        }
+      ],
+      steps: [
+        {
+            step: 1,
+            shortDescription: "TEST DKA",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+        {
+            step: 2,
+            shortDescription: "Mix wet and dry ingredients",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+      ],
+      equipmentTable: [
+        {
+            equipment: 'TEST DKA',
+            subs: 2,
+            equipmentSteps: '1'
+        },
+        {
+            equipment: 'Rolling Pin',
+            subs: 2,
+            equipmentSteps: '2, 3, 4'
+        },
+        {
+            equipment: 'Plastic Wrap',
+            subs: 1,
+            equipmentSteps: '2, 4, 5'
+        },
+      ]
     },
     {
       id: 'u3',
@@ -39,7 +160,67 @@ const Recipes = () => {
       time: 3,
       cost: 2,
       description: "Napolean x NY",
-      author: "Nancy Silverton"
+      author: "Nancy Silverton",
+      cuisine: "Italian",
+      ingredients: [
+        {
+            ingredient: 'TEST BEESTING PIZZA',
+            amount: 3,
+            units: 'Cups',
+            subs: '3'
+        },
+        {
+            ingredient: 'Instant Yeast',
+            amount: '1/3',
+            units: 'Tsp',
+            subs: 2
+        },
+        {
+            ingredient: 'Euro Butter',
+            amount: 1,
+            units: 'Cups',
+            subs: 1
+        }
+      ],
+      steps: [
+        {
+            step: 1,
+            shortDescription: "TEST BEESTING PIZZA",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+        {
+            step: 2,
+            shortDescription: "Mix wet and dry ingredients",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+      ],
+      equipmentTable: [
+        {
+            equipment: 'TEST BEESTING PIZZA',
+            subs: 2,
+            equipmentSteps: '1'
+        },
+        {
+            equipment: 'Rolling Pin',
+            subs: 2,
+            equipmentSteps: '2, 3, 4'
+        },
+        {
+            equipment: 'Plastic Wrap',
+            subs: 1,
+            equipmentSteps: '2, 4, 5'
+        },
+      ]
     },
     {
       id: 'u4',
@@ -50,20 +231,110 @@ const Recipes = () => {
       time: 3,
       cost: 2,
       description: "inspired by xiaolongbao",
-      author: "Zach Pollack"
+      author: "Zach Pollack",
+      cuisine: "Spanish",
+      ingredients: [
+        {
+            ingredient: 'TEST TORTELLINI',
+            amount: 3,
+            units: 'Cups',
+            subs: '3'
+        },
+        {
+            ingredient: 'Instant Yeast',
+            amount: '1/3',
+            units: 'Tsp',
+            subs: 2
+        },
+        {
+            ingredient: 'Euro Butter',
+            amount: 1,
+            units: 'Cups',
+            subs: 1
+        }
+      ],
+      steps: [
+        {
+            step: 1,
+            shortDescription: "TEST TORTELLINI",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+        {
+            step: 2,
+            shortDescription: "TEST TORTELLINI",
+            longDescription: "Mix all dry ingredients first. Then slowly pour wet ingredients in the mixer at low level. Gradually increase to medium level for about 3-4 minutes until the texture is nice and bouncy.",
+            ingredients:['Water','Eggs','Bread Flour'],
+            ingredientsQty: [1,1,3],
+            ingredientsUnits: ['cups','pc','cups'],
+            equipments:['Stand Mixer', 'Silicone Scraper'],
+            image:'https://www.kingarthurflour.com/sites/default/files/blog-images/2016/10/Make-pie-crust-4.jpg'
+        },
+      ],
+      equipmentTable: [
+        {
+            equipment: 'Stand Mixer',
+            subs: 2,
+            equipmentSteps: '1'
+        },
+        {
+            equipment: 'Rolling Pin',
+            subs: 2,
+            equipmentSteps: '2, 3, 4'
+        },
+        {
+            equipment: 'Plastic Wrap',
+            subs: 1,
+            equipmentSteps: '2, 4, 5'
+        },
+      ]
     },
   ];
 
+  const cuisineList = "fRENCH";
+  // const cuisineList = props.cuisineFilter;
+
+  const [open, toggleModal] = React.useState(false);
+  
+  const handleModalOpen = () => {
+    toggleModal(true);
+  };
+
+  const handleModalClose = () => {
+    toggleModal(false);
+  };
+
+  const filteredRecipes = RECIPES.filter((item) => {
+    return item.cuisine.toLowerCase().includes(cuisineList.toLowerCase())
+  })
+
+
   return (
     <div className="main-recipe-page">
+      <FilterBar 
+        //getting data from child
+      />
       <h1>Challenge of the month</h1>
       <div>
-        <RecipeList items={RECIPES}>
+        <RecipeList 
+          items = {filteredRecipes}
+          // items={RECIPES} 
+          onItemclick={handleModalOpen} 
+          open={open} 
+          handleModalClose={handleModalClose}
+          // ingredients={INGREDIENTS}
+          // steps={STEPS}
+          // equipmentTable={equipmentTable}
+        >
           {/* <RecipeModal /> */}
         </RecipeList>
       </div>
       <br></br>
-      <RecipeModal />
+      {/* <RecipeModal onClose={handleModalClose} open={open}/> */}
       <h1>Trending</h1>
       <div>
         <RecipeList items={RECIPES} />
